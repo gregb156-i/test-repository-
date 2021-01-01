ec8ab7208cfd8f8dc4a030ac119fcbd11bfb14a8
gregb156 <51679615+gregb156@users.noreply.github.com>
2021-01-01 20:28:18 +0000

    amendment app.py 02)
    git add *
    clear

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from resources.item import Item, Itemlist
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_ULR','sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'greg'
